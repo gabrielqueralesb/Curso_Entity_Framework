3ea39450ff54a1b300fd2cd028e73436dd67ae8c
Gabriel Querales <gabrielqueralesb@outlook.com>
2023-01-16 16:15:01 -0300

    Add project files.

---
+++ Curso_Entity_Framework/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace Curso_Entity_Framework.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriadId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public virtual ICollection <Tarea> Tareas { get; set; }
+    }
+}
---
+++ Curso_Entity_Framework/Models/Tarea.cs
@@ -0,0 +1,21 @@
+﻿namespace Curso_Entity_Framework.Models
+{
+    public class Tarea
+    {
+        public Guid TareaId { get; set; }
+        public Guid CategoriaId { get; set;}
+        public string Titulo { get; set;}
+        public string Descripcion { get; set;}
+
+        public Prioridad PrioridadTarea { get; set;}
+        public DateTime FechaCreacion { get; set;}
+        public Categoria Categoria { get; set;}
+    }
+    public enum Prioridad
+    {
+        Baja,
+        Media,
+        Alta
+    }
+}
+
