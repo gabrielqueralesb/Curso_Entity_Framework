da1823c611b1bca52c9738f6f85ae354bf54cc53
Gabriel Querales <gabrielqueralesb@outlook.com>
2023-01-17 15:27:39 -0300

    Conexion SQL Server 2012 express (Fallida)

--- Curso_Entity_Framework/Program.cs
+++ Curso_Entity_Framework/Program.cs
@@ -1,18 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Curso_Entity_Framework;
-using Microsoft.AspNetCore.Http;
-
 
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
-        var app = builder.Build();
+        //builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+        //builder.Services.AddSqlServer<TareasContext>("Data Source=NB0288\\SQLEXPRESS;Initial Catalog= TareasDb;Trusted_Connection=True; Integrated Security=True"); //"Data Source= NB0288\\SQLEXPRESS;Initial Catalog=TareasDb;user id=sa;Password=1234"
+        builder.Services.AddSqlServer<TareasContext>("Data Source= NB0288\\SQLEXPRESS;Initial Catalog=TareasDb;user id=sa;Password=1234");
+
+var app = builder.Build();
         app.MapGet("/", () => "Hello World");
 
             app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
                 {
                     dbContext.Database.EnsureCreated();
-                    return Results.Ok("Base de datos en memoria" + dbContext.Database.IsInMemory());
+                    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
                 });
 
             app.Run();
