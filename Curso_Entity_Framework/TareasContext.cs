02c96dcfe50db015457e0bea5fd1d84dd3b23392
Gabriel Querales <gabrielqueralesb@outlook.com>
2023-01-18 15:42:21 -0300

    Clase 16---Conexion a Base de Datos con Fluent API

--- Curso_Entity_Framework/TareasContext.cs
+++ Curso_Entity_Framework/TareasContext.cs
@@ -10,7 +10,24 @@ namespace Curso_Entity_Framework
         public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<Categoria>(categoria =>
+            {
+                categoria.ToTable("Categoria");
+                categoria.HasKey(p => p.CategoriadId);
+                categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
+                categoria.Property(p => p.Descripcion);
+            });
+            modelBuilder.Entity<Tarea>(tarea =>
+            {
+                tarea.ToTable("Tarea");
+                tarea.HasKey(p => p.TareaId);
+                tarea.HasOne(p => p.Categoria).WithMany(p => p.Tareas).HasForeignKey(p=>p.CategoriaId); //relaciona Categoria con Tares a traves del ID
+                tarea.Property(p=>p.Titulo).IsRequired().HasMaxLength(200);
+                tarea.Property(p => p.Descripcion);
+                tarea.Property(p => p.PrioridadTarea);
+                tarea.Property(p => p.FechaCreacion);
+                tarea.Ignore(p => p.Resumen);
+            });
         }
     }
 }
